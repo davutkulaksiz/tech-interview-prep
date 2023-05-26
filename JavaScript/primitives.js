435c85fdbe6df241cd8eeb820a61c1180e3e9999
Davut Kulaksız <davut.kulaksiz.n7@gmail.com>
2023-05-26 10:09:04 +0300

    feat: explanation of primitives

---
+++ JavaScript/primitives.js
@@ -0,0 +1,19 @@
+// Primitives they are immutable
+const boolean = true;
+const null_null = null;
+const undefined = undefined;
+const Number = 1241;
+const BigInt = BigInt(9007199254740991);
+const String = "gatto";
+const Symbol = Symbol("kedi");
+
+console.log(null === undefined)
+
+// undefined is the default value that you'll see for a variable that doesn't have a value
+// assigned to it or for a function that just returns nothing
+
+// null is a similar in a sense that represents an empty value but it's something that
+// you the developer would assign explicitly
+
+// ANYTHING IS NOT A PRIMITIVE
+// IS AN OBJECT

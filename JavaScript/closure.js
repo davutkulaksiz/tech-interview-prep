6d70e386a45cb40a9f0675558b7dd1d6c0adc354
Davut Kulaksız <davut.kulaksiz.n7@gmail.com>
2023-05-26 09:19:10 +0300

    feat: add in-depth explanations for closure

--- JavaScript/closure.js
+++ JavaScript/closure.js
@@ -1,6 +1,39 @@
 // Function that can access values outside of their own curly braces
 // in other words, captures the lexical environment
 
+// NO EXTERNAL DATA
+// NOT A CLOSURE
+// Fully self contained, closed expression
+function pureFun(a, b) {
+  return a + b;
+}
+
+// Function references data outside of its own scope
+// This can be global environment or an outer function
+
+// THIS IS A CLOSURE
+
+// In order for the interpreter to call this function and also know the value of these free variables
+// it creates a closure to store them in a place in memory (heap)
+let b = 3;
+function impureFun(a) {
+  return a + b;
+}
+
+// Closure is a function combined with its outer state or lexical environment
+// require more memory and processing power
+
+// self-contained
+function outer() {
+  let state = "🐈";
+
+  function inner() {
+    return `Hello ${state}`;
+  }
+
+  return inner;
+}
+
 // What is the output?
 var num = 4;
 function outer() {
@@ -13,4 +46,19 @@ function outer() {
   inner();
 }
 
-outer();
+outer(); // logs 3
+
+// What does this code log out?
+for (var i = 0; i < 3; i++) {
+
+  // This is not a pure function because it depends on a variable outside of its scope
+  // Therefore it is closure
+  const log = () => {
+    console.log(i);
+  };
+
+  setTimeout(log, 100);
+}
+
+// 3, 3, 3
+// var get hoisted to parent scope (in this case to global scope)

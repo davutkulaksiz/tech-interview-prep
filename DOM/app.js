04f98ac7e223d94ca32a4f2da11b6bcbf4936e90
Davut Kulaksız <davut.kulaksiz.n7@gmail.com>
2023-05-27 19:41:21 +0300

    feat(DOM): add snippets about how to style elements

--- DOM/app.js
+++ DOM/app.js
@@ -18,4 +18,19 @@ console.log(containers);
 const secondListItem = document.querySelector("ul li:nth-child(2)");
 console.log(secondListItem);
 
+// STYLING ELEMENTS
+// Select Main Heading and Update the Styling
+const mainHeading = document.querySelector("#main-heading");
+console.log(mainHeading.outerHTML); // <h1 id="main-heading" style="color: gray;">Favorite Movie Franchise</h1>
+console.log(mainHeading.innerHTML); // Favorite Movie Franchise
+mainHeading.style.color = "gray";
+
+// Select Multiple Elements and Update the Styling
+const list = document.querySelectorAll(".list-items");
+console.log(list);
+
+for (let i = 0; i < list.length; i++) {
+  list[i].style.fontWeight = "700";
+}
+
 

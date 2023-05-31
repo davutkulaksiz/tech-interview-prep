4647def60dd3b9d24d16b0c299c780a21886660e
Davut Kulaksız <davut.kulaksiz.n7@gmail.com>
2023-05-31 14:10:54 +0300

    feat(data-structures): implement queue and stack with arrays

---
+++ JavaScript/data structures/queue.js
@@ -0,0 +1,7 @@
+// Queue implementation using an array
+let queue = [];
+queue.push("apple");
+queue.push("banana");
+queue.push("orange");
+
+console.log(queue.shift()); // Output: "apple"
---
+++ JavaScript/data structures/stack.js
@@ -0,0 +1,7 @@
+// Stack implementation using an array
+let stack = [];
+stack.push("apple");
+stack.push("banana");
+stack.push("orange");
+
+console.log(stack.pop()); // Output: "orange"

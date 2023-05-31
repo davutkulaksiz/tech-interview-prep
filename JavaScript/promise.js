d2f5241492c0eaec111a8c258063d1a92dd27954
Davut Kulaksız <davut.kulaksiz.n7@gmail.com>
2023-05-31 20:27:11 +0300

    fix(JavaScript): improve the semantics

--- JavaScript/promise.js
+++ JavaScript/promise.js
@@ -1,16 +1,10 @@
 const isCatAngry = true;
 
-const pet = new Promise((resolve, reject) => {
+const petCat = new Promise((resolve, reject) => {
   if (isCatAngry) {
     reject("The cat ran away!");
   }
-  resolve("You pet the cat.");
+  resolve("You've successfully petted the cat!");
 });
 
-pet
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+petCat.then((res) => console.log(res)).catch((err) => console.error(err));

ea727645ac0864e3c40a7fe0d4d43717a55357f1
Davut Kulaksız <davut.kulaksiz.n7@gmail.com>
2023-06-05 13:01:10 +0300

    fix(slot-machine): the reels array did not have dynamic creation logic

--- JavaScript/slot-machine/project.js
+++ JavaScript/slot-machine/project.js
@@ -69,8 +69,9 @@ const spin = () => {
     }
   }
 
-  const reels = [[], [], []];
+  const reels = [];
   for (let i = 0; i < COLS; i++) {
+    reels.push([]);
     const reelSymbols = [...symbols];
     for (let j = 0; j < ROWS; j++) {
       const randomIndex = Math.floor(Math.random() * reelSymbols.length);
@@ -82,10 +83,7 @@ const spin = () => {
   return reels;
 };
 
-spin();
-
 const reels = spin();
-console.log(reels);
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);

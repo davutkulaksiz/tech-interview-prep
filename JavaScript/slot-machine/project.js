aaeec18aa873128f24f59bd843da629db28b26a0
Davut Kulaksız <davut.kulaksiz.n7@gmail.com>
2023-06-05 11:24:00 +0300

    feat(slot-machine): implement deposit function

---
+++ JavaScript/slot-machine/project.js
@@ -0,0 +1,15 @@
+// 1. Deposit Money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Gie the user their winnings
+// 7. Play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+  const depositAmount = prompt("Enter a deposit amount: ");
+};
+
+deposit();
